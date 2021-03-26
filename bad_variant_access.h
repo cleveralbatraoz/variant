103e542e39652675dde50daba258bbe0452686b1
Dmitry Galchenko <dggrus@gmail.com>
2021-03-26 21:40:35 +0300

    minor fixes

--- bad_variant_access.h
+++ bad_variant_access.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <stdexcept>
+#include <exception>
 
-struct bad_variant_access : std::runtime_error {
-  using base = std::runtime_error;
-  using base::base;
-  using base::operator=;
-
-  bad_variant_access() : base("Bad variant access") {}
+struct bad_variant_access : std::exception {
+  char const *what() const noexcept override { return "Bad variant access."; }
 };
